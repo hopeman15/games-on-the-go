84d69a4eeaed083a92ab3341a1acca83d54652c8
Kyle <[hopeman15@users.noreply.github.com]>
2022-07-24 21:42:52 +0200

    [Enhancement] BuildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
@@ -7,29 +8,37 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
-        classpath("com.android.tools.build:gradle:7.2.1")
+        // Android
+        classpath(Dependencies.Gradle.androidGradlePlugin)
+
+        // Kotlin
+        classpath(Dependencies.Gradle.kotlin)
     }
 }
 
 plugins {
-    id("org.jetbrains.kotlinx.kover") version "0.5.1"
+    id(Dependencies.Gradle.kover) version Dependencies.Versions.kover
+    id(Dependencies.Gradle.versions) version Dependencies.Versions.gradleVersions
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+        configurations.all {
+            resolutionStrategy.force(Dependencies.Kotlin.reflect)
+            resolutionStrategy.force(Dependencies.Kotlin.stdlib)
+        }
     }
 }
 
-tasks.register("clean", Delete::class) {
+tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = Dependencies.Versions.jvmTarget
     }
 }
 
@@ -44,7 +53,14 @@ tasks.koverXmlReport {
 }
 
 kover {
-    jacocoEngineVersion.set("0.8.8")
+    jacocoEngineVersion.set(Dependencies.Versions.jacoco)
     isDisabled = false
     generateReportOnCheck = true
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+    outputFormatter = "html"
 }
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,59 @@
+import java.util.Locale
+
+object Dependencies {
+    object Versions {
+        const val agp = "7.2.1"
+        const val androidx = "1.4.2"
+        const val constraint = "2.1.4"
+        const val gradleVersions = "0.42.0"
+        const val jacoco = "0.8.8"
+        const val jvmTarget = "11"
+        const val kotlin = "1.7.0"
+        const val material = "1.6.1"
+
+        // Quality Gates
+        const val kover = "0.5.1"
+    }
+
+    object Android {
+        const val versionMajor = 0
+        const val versionMinor = 1
+        const val versionPatch = 0
+
+        const val compileSdk = 32
+        const val buildToolsVersion = "31.0.0"
+
+        const val minSdk = 23
+        const val targetSdk = 32
+
+        const val material = "com.google.android.material:material:${Versions.material}"
+    }
+
+    object Androidx {
+        const val appcompat = "androidx.appcompat:appcompat:${Versions.androidx}"
+        const val constraint = "androidx.constraintlayout:constraintlayout:${Versions.constraint}"
+    }
+
+    object Gradle {
+        const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.agp}"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+
+        // Quality Gates
+        const val kover = "org.jetbrains.kotlinx.kover"
+        const val versions = "com.github.ben-manes.versions"
+    }
+
+    object Kotlin {
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    }
+
+    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.9.1"
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = setOf("RELEASE", "FINAL", "GA").any { version.toUpperCase(Locale.ROOT).contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
