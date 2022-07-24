bdda832514f1d945fff48f65cf9c6ab238dde949
Kyle <[hopeman15@users.noreply.github.com]>
2022-07-24 15:00:37 +0200

    flavorize

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,17 +5,68 @@ plugins {
 
 android {
     compileSdk = 32
+
+    val ciBuild = System.getenv("CI") == "true"
     defaultConfig {
         applicationId = "com.hello.curiosity.gotg.android"
         minSdk = 21
         targetSdk = 32
+
         versionCode = 1
+        if (ciBuild) {
+            versionCode = System.getenv("GITHUB_RUN_NUMBER").toInt()
+        }
+
         versionName = "1.0"
+
+        multiDexEnabled = true
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // Clear app state after each test
+        testInstrumentationRunnerArguments += mapOf("clearPackageData" to "true")
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
+    }
+
+    flavorDimensions.addAll(listOf("all"))
+    productFlavors {
+        create("production") {
+            dimension = "all"
+        }
+        create("staging") {
+            dimension = "all"
+            versionNameSuffix = "-staging"
+            applicationIdSuffix = ".staging"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+        unitTests.isReturnDefaultValues = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    lint {
+        checkDependencies = true
+        abortOnError = true
+        showAll = true
+        textReport = true
+        xmlReport = false
+        htmlReport = true
+        warningsAsErrors = true
+        disable += mutableSetOf("GoogleAppIndexingWarning", "GradleDependency", "ObsoleteLintCustomCheck")
     }
 }
 
